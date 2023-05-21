09b507bd24103fdc740424b608c9a8f10ffb5f55
Eduard Golyshev <tula71ya@mail.ru>
2023-05-21 12:13:06 +0300

    Создали конструктор

--- snake-app/SnakeApp/Point.cs
+++ snake-app/SnakeApp/Point.cs
@@ -12,6 +12,17 @@ namespace SnakeApp
         public int y;
         public char sym;
 
+        //Конструктор.Инкапсуляция
+        public Point()
+        {  
+        }
+        public Point(int _x, int _y, char _sym)
+        {
+            x = _x;
+            y = _y;
+            sym = _sym;
+        }
+
         public void Draw()
         {
             Console.SetCursorPosition(x, y);
