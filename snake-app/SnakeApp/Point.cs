b6eb042adb9c2e879abee885d2322f4160d1fedc
Eduard Golyshev <tula71ya@mail.ru>
2023-05-23 12:35:52 +0300

    Заставили змейку есть еду

--- snake-app/SnakeApp/Point.cs
+++ snake-app/SnakeApp/Point.cs
@@ -47,6 +47,10 @@ namespace SnakeApp
                 y += offset;
             }
         }
+        public bool IsHit(Point p)
+        {
+            return p.x == this.x && p.y == this.y;
+        }
 
         public void Draw()
         {
